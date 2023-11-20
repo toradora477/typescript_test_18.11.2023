0373e51262c6f3c1c5582aa1b38b478e363acd4c
Dima Kryshtopa <toradora477@gmail.com>
2023-11-20 17:53:02 +0200

    fix

---
+++ projects/react-app/src/App/App.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import './App.css';
+
+const App: React.FC = () => {
+  return (
+    <div className="app-container">
+      <h1 className="app-title">Hello, React App!</h1>
+    </div>
+  );
+};
+
+export default App;
